b447748d1d8be749cb9eb724a1ea18fb995c5789
Ascecent <andres85044@gmail.com>
2023-05-20 19:16:44 -0500

    Adding Material UI ThemeProvider And Essential Wrappers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import { ThemeProvider } from '@mui/material';
+
+import getCustomTheme from './theme';
+
 import App from './components/App';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={getCustomTheme()}>
+			<StyledEngineProvider injectFirst>
+				<CssBaseline />
+				<App />
+			</StyledEngineProvider>
+		</ThemeProvider>
 	</React.StrictMode>
 );
